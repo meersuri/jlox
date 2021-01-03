2e4d9328e50877972ce5e0be43738aeb3d65f7bc
meersuri <meersuri@gmail.com>
2021-01-03 20:06:39 +0800

    added code for identifiers

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class Scanner {
 
     private final String source;
@@ -12,6 +14,28 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+    private static final Map<String, TokenType> keywords;
+
+    static
+    {
+        keywords = new HashMap<>();
+        keywords.put("and", TokenType.AND);
+        keywords.put("class", TokenType.CLASS);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("false", TokenType.FALSE);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("fun", TokenType.FUN);
+        keywords.put("if", TokenType.IF);
+        keywords.put("nil", TokenType.NIL);
+        keywords.put("or", TokenType.OR);
+        keywords.put("print", TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super", TokenType.SUPER);
+        keywords.put("this", TokenType.THIS);
+        keywords.put("true", TokenType.TRUE);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("while", TokenType.WHILE);
+    }
 
     Scanner(String source)
     {
@@ -118,6 +142,35 @@ public class Scanner {
         addToken(TokenType.NUMBER, value);
     }
 
+    private boolean isAlpha(char c)
+    {
+        if (c >= 'a' && c <= 'z')
+            return true;
+        else if (c >= 'A' && c <= 'Z')
+            return true;
+        else if (c == '_')
+            return true;
+        return false;
+    }
+
+    private boolean isAlphanumeric(char c)
+    {
+        return (isAlpha(c) || isDigit(c));
+    }
+
+    private void identifier()
+    {
+        while (isAlphanumeric(peek()))
+            advance();
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null)
+            type = TokenType.IDENTIFIER;
+        addToken(type);
+        return;
+        
+    }
+
     private void scanToken()
     {
         char c = advance();
@@ -188,6 +241,8 @@ public class Scanner {
             default:
                 if (isDigit(c))
                     number();
+                else if (isAlpha(c))
+                    identifier();
                 else
                 {
                     Lox.error(line, "Unexpected character.");
